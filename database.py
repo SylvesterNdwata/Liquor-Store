7747cb080a53575ddc0fed45535c7419b737f1ff
sylvester ndwata <ndwatasylvester88gmail.com>
2025-11-26 11:25:56 +0100

    Cart Logic

--- database.py
+++ database.py
@@ -5,12 +5,19 @@ class Database:
     def __init__(self, db_name):
         self.con = sql.connect(db_name)
         self.cur = self.con.cursor()
+        self.customer_name = ""
         
     
     def create_tables(self):
-        self.cur.execute("CREATE TABLE IF NOT EXISTS products (id INTEGER PRIMARY KEY, name TEXT, category TEXT, price REAL)")
-        self.cur.execute("CREATE TABLE IF NOT EXISTS sales (id INTEGER PRIMARY KEY, product_id INTEGER, quantity INTEGER, sale_date TEXT, FOREIGN KEY(product_id) REFERENCES products(id))")
+        self.cur.execute("CREATE TABLE IF NOT EXISTS products (id INTEGER PRIMARY KEY, name TEXT, category TEXT, stock INT, price REAL)")
+        self.cur.execute("CREATE TABLE IF NOT EXISTS sales (id INTEGER PRIMARY KEY, product_id INTEGER, customer_id INTEGER, quantity INTEGER, sale_date TIMESTAMP, FOREIGN KEY(product_id) REFERENCES products(id), FOREIGN KEY(customer_id) REFERENCES customer(id))")
         self.con.commit()
+    
+    def customer_table(self):
+        self.cur.execute(f"CREATE TABLE IF NOT EXISTS customer (id INTEGER PRIMARY KEY, name TEXT, age INTEGER)")
+        
+    def insert_customer(self, name, age):
+        self.cur.execute("INSERT INTO customer (name, vorname) VALUES (?, ?)", (name, age))
         
     def insert_product(self, name, category, price):
         self.cur.execute("INSERT INTO products (name, category, price) VALUES (?, ?, ?)", (name, category, price))
@@ -31,3 +38,9 @@ class Database:
         data = self.res.fetchall()
         return pd.DataFrame(data, columns=['id', 'product_id', 'quantity', 'sale_date'])
         self.con.commit()
+        
+    def fetch_customer(self):
+        self.res = self.cur.execute("SELECT * FROM customer")
+        data = self.res.fetchall()
+        return pd.DataFrame(data, columns=['name', 'vorname'])
+        
--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from database import Database
 from invoice import Invoice
+import pandas as pd
+from datetime import datetime
 
 database = Database('LiquorStore.db')
+
+database.customer_table()
 
 print("Welcome to the Liquor Store")
 
@@ -9,49 +13,54 @@ invoce = Invoice()
 
 customer_invoice = invoce.items
 
-customer_name = input("Please enter your name: ").title()
+database.customer_name = input("Please enter your name: ").title()
 
 customer_age = int(input("Please input your age: "))
+
+x = datetime.now()
+date = x.strftime("%x")
+print(date)
 
 if customer_age < 18:
     print("You are not liable to purchase our products, must be 18 or older")
     exit()
 elif customer_age >= 18:
- print(f"Hello, {customer_name}! Here are our available products:")
+ print(f"Hello, {database.customer_name}! Here are our available products:")
 
 print(database.fetch_products())
 
-product_name = input("Please enter the name of the product you wish to purchase: ").title()
 
-while True:
+
+cart = {}
+
+shop = True
+while shop:
+    product_name = input("Please enter the name of the product you wish to purchase: ").title()
+    product_quantity = int(input(f"How many units of {product_name} would you like to buy? "))
     if product_name not in database.fetch_products().name.values:
         print("Sorry, we do not have that product available.")
     elif product_name in database.fetch_products().name.values and customer_age >= 18:
         product = database.fetch_products()[database.fetch_products().name == product_name]
-        product_id = database.fetch_products()[database.fetch_products().name == product_name].id.item()
+        product_id = product.id.item()
         price = product.price.item()
         print(f"The price of {product_name} is ${price} per unit.")
         customer_invoice.append(product_name)
         invoce.add_item(price)
+        if product_name in cart:
+            cart[f"{product_name}"] += product_quantity
+        elif product_name not in cart:
+            cart[f"{product_name}"] = product_quantity
+        # database.insert_customer(name=database.customer_name, age=customer_age)
+        # database.insert_sale(product_id=product_id, sale_date=date, )
         print(f"{product_name} has been added to your invoice.")
-        more_products = input("Would you like to purchase another product? (yes/no): ").lower()
-        if more_products == "no":
-            print("Thank you for your purchase!")
-            print(f"Your invoice items: {customer_invoice}")
-            print(f"Total cost: ${invoce.total_cost}")
-            break
-        elif more_products == "yes":
-            product_name = input("Please enter the name of the product you wish to purchase: ").title()
-            if product_name not in database.fetch_products().name.values:
-                print("Sorry, we do not have that product available.")
-            elif product_name in database.fetch_products().name.values:
-                product = database.fetch_products()[database.fetch_products().name == product_name]
-                product_id = database.fetch_products()[database.fetch_products().name == product_name].id.item()
-                price = product.price.item()
-                print(f"The price of {product_name} id ${price} per unit.")
-                customer_invoice.append(product_name)
-                invoce.add_item(price)
-                print(f"{product_name} has been added to your invoice.")
+    add_to_cart = input("Would you like to keep shopping? Yes or No?: ")
+    if add_to_cart == "yes":
+        shop = True
+    elif add_to_cart == "no":
+        shop = False
+    print(cart)
+            
+ 
             
 
 
